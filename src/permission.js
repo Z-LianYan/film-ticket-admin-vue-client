3bdaff916d6f00cd92c38cb2eaf38cf147c0d888
zly <1371935318@qq.com>
2022-05-14 15:33:24 +0800

    更换静态资源域名

--- src/permission.js
+++ src/permission.js
@@ -1,44 +1,45 @@
-import router from '@/router'
-import store from '@/store'
-import { Message } from 'element-ui'
-// import NProgress from 'nprogress' // progress bar
-// import 'nprogress/nprogress.css' // progress bar style
-import getPageTitle from '@/utils/get-page-title'
-import { getToken, getUserInfo, routerMenuFilter } from '@/common/tools'
+import router from "@/router";
+import store from "@/store";
+import { Message } from "element-ui";
+import NProgress from "nprogress"; // progress bar
+import "nprogress/nprogress.css"; // progress bar style
+import getPageTitle from "@/utils/get-page-title";
+import { getToken, getUserInfo, routerMenuFilter } from "@/common/tools";
 
+NProgress.configure({ showSpinner: false }); // NProgress Configuration
 
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
-
-const whiteList = ['/login'] // no redirect whitelist
-
+const whiteList = ["/login"]; // no redirect whitelist
 
 router.beforeEach((to, from, next) => {
-  NProgress.start()
-  document.title = getPageTitle(to.meta.title)
-  const hasToken = getToken()
+  NProgress.start();
+  document.title = getPageTitle(to.meta.title);
+  const hasToken = getToken();
   if (hasToken) {
-    if (to.path === '/login') {
-      next({ path: '/' })
-      NProgress.done()
+    if (to.path === "/login") {
+      next({ path: "/" });
+      NProgress.done();
     } else {
-      if(!store.state.accessMenu.routerMenu.length && store.state.accessMenu.isLoadingMenu && to.path !== '/404') {
+      if (
+        !store.state.accessMenu.routerMenu.length &&
+        store.state.accessMenu.isLoadingMenu &&
+        to.path !== "/404"
+      ) {
         store.state.accessMenu.isLoadingMenu = false;
         store.dispatch("accessMenu/getAccessMenu");
       }
-      next()
+      next();
     }
   } else {
     if (whiteList.indexOf(to.path) !== -1) {
-      next()
+      next();
     } else {
-      next('/login')
-      NProgress.done()
+      next("/login");
+      NProgress.done();
     }
   }
-})
-
+});
 
 router.afterEach(() => {
   // finish progress bar
-  NProgress.done()
-})
+  NProgress.done();
+});
